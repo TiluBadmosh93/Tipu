a3549fa80df853d6498ba4a741970429844ae177
ᏯᎯᎲᎬᏁ ᎬᎷᏬ <tilubadmosh9@gmail.com>
2024-12-01 17:08:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @MrRanDom8
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7690517391:AAEUAZVGQogBq2xqbisb5BbKMM7dgCswCdw")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("MrRanDom8")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7285133256")
+
+
+
+
+  
